6641e8aeb5fd41f2ee52f97bbbd6b4e204789d50
PhoopPeep <christina_c@cmu.ac.th>
2024-07-12 04:10:33 +0700

    finish!

---
+++ lab02/src/views/EventListView.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts">
+import EventCard from '@/components/EventCard.vue';
+import Event from '@/types/Event';
+import CategoryOrganizer from '@/components/CategoryOrganizer.vue';
+import { ref, onMounted } from 'vue';
+import EventService from '@/services/EventService';
+import { error } from 'console';
+
+const events = ref<Event[]>(null)
+
+onMounted(() => {
+  EventService.getEvents()
+  .then((response) => {
+    events.value = response.data
+  })
+  .catch((error) => {
+    console.error('There was an error!', error)
+  })
+})
+</script>
+
+<template>
+  <div class="events">
+    <h1>Events For Good</h1>
+    <!-- new element -->
+    <EventCard v-for="event in events" key="event.id" :event="event" />
+  </div>
+</template>
+
+<style scoped>
+.events {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+</style>
